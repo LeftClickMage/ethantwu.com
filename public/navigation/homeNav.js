1323edca75eaa9c955a556d2ae5c1dc19ae6de2f
fairopshotgun <leftclickmage@gmail.com>
2022-11-26 14:17:10 -0500

    workupdate

--- public/navigation/homeNav.js
+++ public/navigation/homeNav.js
@@ -18,11 +18,11 @@ document.write(`
   </style>
   <nav class="navbar navbar-expand-md navbar-light p-0 m-0 position-fixed w-100 top-0" id="navigationBar">
   <div class="container-fluid">
-    <a class="navbar-brand repathHREF" href="/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home">
-    <img src="/public/logos/lcm-logo.png" width="70px" class="repathIMG">
+    <a class="navbar-brand" href="https://ethantwu.com/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home">
+    <img src="public/logos/lcm-logo.png" width="70px" class="repathIMG">
     </a>
     <a class="navbar-brand" href="https://smasher.ethantwu.com" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Smasher Minigame">
-      <img src="/public/logos/smasher-logo.png" width="70px" class="repathIMG">
+      <img src="public/logos/smasher-logo.png" width="70px" class="repathIMG">
       </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -30,10 +30,10 @@ document.write(`
       <div class="navbar-collapse collapse" id="collapse">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link font-increase-2x repathHREF" href="/fairopbot.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="FairOPBot" id="discordBot">Discord Bot</a>
+            <a class="nav-link font-increase-2x" href="https://ethantwu.com/fairopbot.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="FairOPBot" id="discordBot">Discord Bot</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link font-increase-2x repathHREF" href="/minigames/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Minigames" id="minigames">Minigames</a>
+            <a class="nav-link font-increase-2x" href="https://ethantwu.com/minigames/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Minigames" id="minigames">Minigames</a>
           </li>
          
         </ul>
@@ -45,43 +45,7 @@ document.write(`
 
 `);
 
-var spliter = String(window.location);
-spliter = spliter.split('/')
-while(true){
-  if (spliter[spliter.length-1] != "www.ethantwu.com" ||spliter[spliter.length-1] != "ethantwu.com" ){
-    spliter.pop();
-  } else {
-    break;
-  }
-}
-var path;
-for(let i = 0; i < spliter.length; i++){
-  path += spliter[i] + "/";
-}
-path = path.replace("undefined", "");
-path = path.substring(0, path.length-1);
 
-const outer = document.querySelectorAll('.repathIMG');
-for(let i = 0; i<outer.length; i++){
-
-  var src=outer[i].src;
-  if (src.startsWith("file://")){
-    src= src.replace("file://", "");
-  }
-  outer[i].setAttribute("src", path+src);
-
-  }
-const inner = document.querySelectorAll('.repathHREF');
-for(let i = 0; i<inner.length; i++){
-
-   var href=inner[i].getAttribute('href');
-   if(href.startsWith("file://")){
-    href= href.replace("file://", "");
-   }
-    inner[i].setAttribute("href", path+href);
-
-  }
-  
 
 
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
