2da9cdc7d1c459a1679e6976ed2daebaec77e56b
fairopshotgun <leftclickmage@gmail.com>
2022-11-26 10:33:39 -0500

    minigames

--- public/navigation/homeNav.js
+++ public/navigation/homeNav.js
@@ -1,3 +1,4 @@
+
 document.write(`
   <style>
   
@@ -17,11 +18,11 @@ document.write(`
   </style>
   <nav class="navbar navbar-expand-md navbar-light p-0 m-0 position-fixed w-100 top-0" id="navigationBar">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home">
-    <img src="public/logos/lcm-logo.png" width="70px">
+    <a class="navbar-brand repathHREF" href="/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home">
+    <img src="/public/logos/lcm-logo.png" width="70px" class="repathIMG">
     </a>
     <a class="navbar-brand" href="https://smasher.ethantwu.com" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Smasher Minigame">
-      <img src="public/logos/smasher-logo.png" width="70px">
+      <img src="/public/logos/smasher-logo.png" width="70px" class="repathIMG">
       </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -29,10 +30,10 @@ document.write(`
       <div class="navbar-collapse collapse" id="collapse">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link font-increase-2x" href="fairopbot.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="FairOPBot" id="discordBot">Discord Bot</a>
+            <a class="nav-link font-increase-2x repathHREF" href="/fairopbot.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="FairOPBot" id="discordBot">Discord Bot</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link font-increase-2x" href="#">[Item]</a>
+            <a class="nav-link font-increase-2x repathHREF" href="/minigames/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Minigames" id="minigames">Minigames</a>
           </li>
          
         </ul>
@@ -44,11 +45,47 @@ document.write(`
 
 `);
 
+var spliter = String(window.location);
+spliter = spliter.split('/')
+while(true){
+  if (spliter[spliter.length-1] != "ethantwu.com"){
+    spliter.pop();
+  } else {
+    break;
+  }
+}
+var path;
+for(let i = 0; i < spliter.length; i++){
+  path += spliter[i] + "/";
+}
+path = path.replace("undefined", "");
+path = path.substring(0, path.length-1);
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const outer = document.querySelectorAll('.repathIMG');
+for(let i = 0; i<outer.length; i++){
+
+  var src=outer[i].src;
+  src= src.replace("file://", "");
+  outer[i].setAttribute("src", path+src);
+
+  }
+const inner = document.querySelectorAll('.repathHREF');
+for(let i = 0; i<inner.length; i++){
+
+   var href=inner[i].getAttribute('href');
+    href= href.replace("file://", "");
+    inner[i].setAttribute("href", path+href);
+
+  }
+  
+
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
   })
   if(document.title == "Ethan Twu - FairOPBot"){
     document.getElementById('discordBot').classList.add("active");
+  } else if(document.title == "Ethan Twu - Minigames"){
+    document.getElementById('minigames').classList.add("active");
   }
