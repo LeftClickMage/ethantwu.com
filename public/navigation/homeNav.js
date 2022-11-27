8796b09fbc2d9e6086d85ce821dcbfe5726e9306
fairopshotgun <leftclickmage@gmail.com>
2022-11-26 21:57:56 -0500

    minigamePages

--- public/navigation/homeNav.js
+++ public/navigation/homeNav.js
@@ -1,3 +1,5 @@
+var penguinPath = "file:///media/fuse/crostini_e202c9d5225290161ded36479c47b60d686ceb00_termina_penguin/ethantwu.com/";
+// var penguinPath = "https://ethantwu.com/";
 
 document.write(`
   <style>
@@ -15,14 +17,17 @@ document.write(`
         background-color:rgba(248,249,250,1);
         transition: background-color 500ms;
       }
+      .tooltip-inner {
+    max-width: 100% !important;
+}
   </style>
   <nav class="navbar navbar-expand-md navbar-light p-0 m-0 position-fixed w-100 top-0" id="navigationBar">
   <div class="container-fluid">
-    <a class="navbar-brand" href="https://ethantwu.com/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home">
-    <img src="public/logos/lcm-logo.png" width="70px" class="repathIMG">
+    <a class="navbar-brand hrefRepath" href="index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home">
+    <img src="public/logos/lcm-logo.png" width="70px" class="imgRepath">
     </a>
-    <a class="navbar-brand" href="https://smasher.ethantwu.com" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Smasher Minigame">
-      <img src="public/logos/smasher-logo.png" width="70px" class="repathIMG">
+    <a class="navbar-brand" href="https://smasher.ethantwu.com" data-bs-toggle="tooltip"  data-bs-container="body" data-bs-placement="bottom" data-bs-html ="true" title="<span class='badge bg-warning'>BETA</span> Smasher Minigame">
+      <img src="public/logos/smasher-logo.png" width="70px" class="imgRepath">
       </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -30,10 +35,10 @@ document.write(`
       <div class="navbar-collapse collapse" id="collapse">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link font-increase-2x" href="https://ethantwu.com/fairopbot.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="FairOPBot" id="discordBot">Discord Bot</a>
+            <a class="nav-link font-increase-2x hrefRepath" href="fairopbot.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="FairOPBot" id="discordBot">Discord Bot</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link font-increase-2x" href="https://ethantwu.com/minigames/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Minigames" id="minigames">Minigames</a>
+            <a class="nav-link font-increase-2x hrefRepath" href="minigames/index.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Minigames" id="minigames">Minigames</a>
           </li>
          
         </ul>
@@ -44,8 +49,15 @@ document.write(`
 
 
 `);
+var elements = document.querySelectorAll(".hrefRepath");
+for(let i =0; i<elements.length; i++){
+  elements[i].setAttribute("href", penguinPath + elements[i].getAttribute("href"));
+}
 
-
+var elements = document.querySelectorAll(".imgRepath");
+for(let i =0; i<elements.length; i++){
+  elements[i].setAttribute("src", penguinPath + elements[i].getAttribute("src"));
+}
 
 
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
