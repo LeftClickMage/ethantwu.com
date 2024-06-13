467c113ea2d255ad4f7e9a2dcbfc4df86fc1b246
LeftClickMage <leftclickmage@gmail.com>
2024-06-12 21:14:13 -0400

    oops forgot to change penguin path

--- public/navigation/homeNav.js
+++ public/navigation/homeNav.js
@@ -1,5 +1,5 @@
-var penguinPath = "file:///C:/Users/ethan/ethantwu.com-1/";
-//var penguinPath = "https://ethantwu.com/";
+//var penguinPath = "file:///C:/Users/ethan/ethantwu.com-1/";
+var penguinPath = "https://ethantwu.com/";
 
 document.write(`
   <style>
