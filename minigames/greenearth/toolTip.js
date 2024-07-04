53d736f6aef738506429e4209d789ccf13b9a8f6
Ethan Twu <leftclickmage@gmail.com>
2024-07-03 22:59:25 -0400

    Delete minigames/greenearth directory

--- minigames/greenearth/toolTip.js
+++
@@ -1,8 +0,0 @@
-class ToolTip extends Phaser.GameObjects.Rectangle {
-    constructor(scene, x, y, width, height, color){
-        super(scene, x, y, width, height, color);
-        scene.add.existing(this);
-        this.setOrigin(0);
-        this.setAlpha(0);
-  }
-}
