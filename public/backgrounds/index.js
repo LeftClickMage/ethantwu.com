4cb76d24728330aaf55eefe73034263e1f1e02ac
Ethan Twu <leftclickmage@gmail.com>
2024-07-24 09:45:15 -0400

    background update

--- public/backgrounds/index.js
+++ public/backgrounds/index.js
@@ -34,89 +34,54 @@ particles.setAttribute("position", new THREE.BufferAttribute(posArray, 3));
 particlesRed.setAttribute("position", new THREE.BufferAttribute(posArrayRed, 3));
 particlesBlue.setAttribute("position", new THREE.BufferAttribute(posArrayBlue, 3));
 
-const material = new THREE.PointsMaterial({ 
-    size: 0.1,
-}); 
-
 const particleMaterial = new THREE.PointsMaterial({ 
-    size: 0.1,
+    size: 0.12,
     map: texture,
     transparent: true,
     color: "white",
 }); 
 const particleMaterialRed = new THREE.PointsMaterial({ 
-    size: 0.1,
+    size: 0.12,
     map: texture,
     transparent: true,
     color: "#39FF14",
 }); 
 const particleMaterialBlue = new THREE.PointsMaterial({ 
-    size: 0.1,
+    size: 0.12,
     map: texture,
     transparent: true,
     color: "#04D9FF",
 }); 
-const sphere = new THREE.Points( geometry, material );  
-sphere.position.z = -20;
 
 var particlesMesh = new THREE.Points(particles, particleMaterial);
 var particlesMeshRed = new THREE.Points(particlesRed, particleMaterialRed);
 var particlesMeshBlue = new THREE.Points(particlesBlue, particleMaterialBlue);
 
-
 scene.add(particlesMesh, particlesMeshRed, particlesMeshBlue);
 
-
-
 var camera = new THREE.PerspectiveCamera(30, config.width / config.height);
-
-// camera.rotation.x = -0.5;
 scene.add(camera);
-
-
-// const controls = new OrbitControls(camera, render.domElement);
-
-var light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(5, 5, 5);
-scene.add(light);
-
-var light2 = new THREE.PointLight(0xffffff, 1, 100);
-light2.position.set(0, 40, -50);
-scene.add(light2);
+camera.position.z = 20;
 
 var canvas = document.getElementById("overallBackground");
 var render = new THREE.WebGLRenderer({canvas});
 render.setSize(config.width, config.height);
 render.render(scene, camera);
-render.setClearColor(new THREE.Color('#010b19'), 1);
-
+render.setClearColor(new THREE.Color('#000007'), 1);
 
 var renderScene = new RenderPass(scene, camera);
 var composer = new EffectComposer(render);
 composer.addPass(renderScene);
 
-var bloomPass = new UnrealBloomPass(new THREE.Vector2(config.width, config.height), 1.6, 0.5, 0.1);
+var bloomPass = new UnrealBloomPass(new THREE.Vector2(config.width, config.height), 4, 0.3, 0.1);
 composer.addPass(bloomPass);
 render.toneMapping = THREE.CineonToneMapping;
 render.toneMappingExposer = 3;
 
-
-var mouseXPrev = 0;
-
 var clock = new THREE.Clock();
-window.addEventListener("mousemove", animateParticles);
-var mouseX = 0;
-var mouseY = 0;
-function animateParticles(event){
-  mouseY = event.clientY;
-  mouseX = event.clientX;
-}
 
 var stop = false;
 var fps, fpsInterval, startTime, now, then, elapsed;
-
-
-// initialize the timer variables and start the animation
 
 function startAnimating(fps) {
     fpsInterval = 1000 / fps;
@@ -125,63 +90,25 @@ function startAnimating(fps) {
     animate();
 }
 
-
 function animate() {
-
-    // request another frame
-
     requestAnimationFrame(animate);
-
-    // calc elapsed time since last loop
-
     now = Date.now();
     elapsed = now - then;
-
-    // if enough time has elapsed, draw the next frame
-
     if (elapsed > fpsInterval) {
-
-        // Get ready for next frame by setting then=now, but also adjust for your
-        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
         then = now - (elapsed % fpsInterval);
-
-       camera.position.z = 20;
-  // sphere.position.z += 0.5;
-  // alert('test');
-  var elapsedTime = clock.getElapsedTime();
-    config.width = window.innerWidth;
-    config.height = window.innerHeight;
-
-    // render.render(scene, camera);
-    composer.render();
-
-
-particlesMesh.rotation.y += -.1/200;
-particlesMeshRed.rotation.y +=  -.02/100;
-particlesMeshBlue.rotation.y +=  -.25/300;
-particlesMesh.rotation.x += -.1/200;
-particlesMeshRed.rotation.x +=  -.02/100;
-particlesMeshBlue.rotation.x +=  -.25/300;
-// particlesMesh.rotation.x =  (elapsedTime * -.1/2);
-// particlesMeshRed.rotation.x =  (elapsedTime * -.02);
-// particlesMeshBlue.rotation.x =  (elapsedTime * -.25/3);
-
-//     if(mouseX>0){
-//       particlesMesh.rotation.x = -(mouseY);
-//       particlesMesh.rotation.y = -(mouseX);
-//       particlesMeshRed.rotation.x = -(mouseY);
-//       particlesMeshRed.rotation.y = -(mouseX);
-//       particlesMeshBlue.rotation.x = -(mouseY);
-//       particlesMeshBlue.rotation.y = -(mouseX);
-//     } 
-// //       // particlesMesh.rotation.y += 0.01;
-    
-      
-      
-    camera.aspect = config.width/config.height;
-    camera.updateProjectionMatrix();
-    render.setSize(config.width, config.height);
-    
+        var elapsedTime = clock.getElapsedTime();
+        config.width = window.innerWidth;
+        config.height = window.innerHeight;
+        composer.render();
+        particlesMesh.rotation.y += -.1/200;
+        particlesMeshRed.rotation.y +=  -.02/100;
+        particlesMeshBlue.rotation.y +=  -.25/300;
+        particlesMesh.rotation.x += -.1/200;
+        particlesMeshRed.rotation.x +=  -.02/100;
+        particlesMeshBlue.rotation.x +=  -.25/300; 
+        camera.aspect = config.width/config.height;
+        camera.updateProjectionMatrix();
+        render.setSize(config.width, config.height);
     }
 }
 startAnimating(60);
