8bedacf341de0ff02f2b37859c81f25e7cff042a
Ethan Twu <leftclickmage@gmail.com>
2024-08-26 23:11:52 -0400

    im in idot

--- minigames/greenearth/waveTimer.js
+++ minigames/greenearth/waveTimer.js
@@ -56,9 +56,6 @@ class waveTimer extends Phaser.Scene {
                 this.hotbarScene.tutorialActiveTree = true;
             }
         
-            if(player.highestWave < this.mainGame.waveNumber && !this.mainGame.creativeMode){
-                player.highestWave = this.mainGame.waveNumber;
-            }
             if(this.mainGame.waveNumber == 21 && this.shownVictory == false && !this.mainGame.creativeMode && player.highestWave <= 20){
                 this.mainGame.victoryScreenActive = true;
                 this.scene.launch("victoryScreen");
